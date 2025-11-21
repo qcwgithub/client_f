2b425765c069d7cd7c738ec35511d60b9a9ae6d3
qiucw <doitmyway@qq.com>
2025-11-21 09:16:35 +0800

    init project

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,83 @@
+import 'package:client_f/pages/main_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginState();
+}
+
+class _LoginState extends State<LoginPage>{
+  final TextEditingController emailController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 28),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // App Logo / Title
+            Text (
+              "SceneHub",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+                color: Colors.indigo.shade700,
+              )
+            ),
+            
+            const SizedBox(height: 12),
+            
+            const Text(
+              "Join with your email or continue as a guest",
+              style: TextStyle(fontSize: 16, color: Colors.black54)
+            ),
+            
+            const SizedBox(height: 40),
+
+            // Email TextField
+            TextField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: "Email Address",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // Login Button
+            SizedBox(
+              width: double.infinity,
+              child: FilledButton(
+                onPressed: () {
+                  Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) {
+                    return const MainPage();
+                  }));
+                }, 
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 14),
+                  child: Text("Continue"),
+                ),
+              ),
+            ),
+
+            // Guest Mode
+            Center(
+              child: TextButton(
+                onPressed: () {
+                  // 
+                },
+                child: const Text("Continue as Guest")),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,65 @@
+import 'package:client_f/pages/create_page.dart';
+import 'package:client_f/pages/discover_page.dart';
+import 'package:client_f/pages/home_page.dart';
+import 'package:client_f/pages/profile_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MainPageState();
+  }
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+  
+  final List<Widget> pages = const [
+    HomePage(),
+    DiscoverPage(),
+    CreatePage(),
+    ProfilePage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: pages,
+      ),
+      bottomNavigationBar: NavigationBar(
+        height: 65,
+        selectedIndex: currentIndex,
+        onDestinationSelected: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.home_outlined), 
+            selectedIcon: Icon(Icons.home),
+            label: "Home"
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.explore_outlined), 
+            selectedIcon: Icon(Icons.explore),
+            label: "Discover"
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.add_circle_outlined), 
+            selectedIcon: Icon(Icons.add_circle),
+            label: "Create"
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outlined), 
+            selectedIcon: Icon(Icons.person),
+            label: "Profile"
+          ),
+        ]),
+    );
+  }
+}
