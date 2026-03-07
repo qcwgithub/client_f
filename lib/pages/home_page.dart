3d2821eb97041882b0f70d92797538500a82ce57
answerwinner <67001868+answerwinner@users.noreply.github.com>
2026-03-07 12:02:34 +0800

    未读提示

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,4 @@
-import 'contacts_page.dart';
 import 'conversation_list_page.dart';
-import 'explore_page.dart';
 import 'main_page.dart';
 import 'profile_page.dart';
 import 'package:flutter/material.dart';
@@ -16,22 +14,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int currentIndex = 0;
-  
+
   final List<Widget> pages = const [
     ConversationListPage(),
     MainPage(),
-    ContactsPage(),
-    ExplorePage(),
     ProfilePage(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: currentIndex,
-        children: pages,
-      ),
+      body: IndexedStack(index: currentIndex, children: pages),
       bottomNavigationBar: NavigationBar(
         height: 65,
         selectedIndex: currentIndex,
@@ -42,31 +35,22 @@ class _HomePageState extends State<HomePage> {
         },
         destinations: const [
           NavigationDestination(
-            icon: Icon(Icons.chat_outlined), 
+            icon: Icon(Icons.chat_outlined),
             selectedIcon: Icon(Icons.chat),
-            label: "Chat"
+            label: "Chat",
           ),
           NavigationDestination(
-            icon: Icon(Icons.home_outlined), 
+            icon: Icon(Icons.home_outlined),
             selectedIcon: Icon(Icons.home),
-            label: "Home"
+            label: "Home",
           ),
           NavigationDestination(
-            icon: Icon(Icons.contacts_outlined), 
-            selectedIcon: Icon(Icons.contacts),
-            label: "Contacts"
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.explore_outlined), 
-            selectedIcon: Icon(Icons.explore),
-            label: "Explore"
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.person_outlined), 
+            icon: Icon(Icons.person_outlined),
             selectedIcon: Icon(Icons.person),
-            label: "Profile"
+            label: "Profile",
           ),
-        ]),
+        ],
+      ),
     );
   }
 }
